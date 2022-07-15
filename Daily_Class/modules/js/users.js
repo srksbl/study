004a93e0a79631c00079e1a530653aa90d24deb5
srksbl <shahrukhahmad142@gmail.com>
2022-07-15 22:13:56 +0530

    Daily classes push

---
+++ Daily_Class/modules/js/users.js
@@ -0,0 +1,78 @@
+const users = () => {
+  const data = [
+    {
+      id: 1,
+      first_name: "Alta",
+      last_name: "Ridgley",
+      email: "aridgley0@stanford.edu",
+      gender: "Non-binary",
+    },
+    {
+      id: 2,
+      first_name: "Shayne",
+      last_name: "Preddy",
+      email: "spreddy1@usnews.com",
+      gender: "Female",
+    },
+    {
+      id: 3,
+      first_name: "Rebe",
+      last_name: "Revan",
+      email: "rrevan2@instagram.com",
+      gender: "Agender",
+    },
+    {
+      id: 4,
+      first_name: "Junie",
+      last_name: "Wigfield",
+      email: "jwigfield3@opera.com",
+      gender: "Female",
+    },
+    {
+      id: 5,
+      first_name: "Melany",
+      last_name: "Risbrough",
+      email: "mrisbrough4@amazonaws.com",
+      gender: "Female",
+    },
+    {
+      id: 6,
+      first_name: "Harriot",
+      last_name: "Donhardt",
+      email: "hdonhardt5@cbslocal.com",
+      gender: "Female",
+    },
+    {
+      id: 7,
+      first_name: "Caresse",
+      last_name: "Chilcott",
+      email: "cchilcott6@nps.gov",
+      gender: "Non-binary",
+    },
+    {
+      id: 8,
+      first_name: "Lamar",
+      last_name: "Legg",
+      email: "llegg7@dot.gov",
+      gender: "Male",
+    },
+    {
+      id: 9,
+      first_name: "Efrem",
+      last_name: "Scowen",
+      email: "escowen8@privacy.gov.au",
+      gender: "Male",
+    },
+    {
+      id: 10,
+      first_name: "Dominga",
+      last_name: "Greveson",
+      email: "dgreveson9@tiny.cc",
+      gender: "Female",
+    },
+  ];
+  return data;
+};
+
+
+export default users;
